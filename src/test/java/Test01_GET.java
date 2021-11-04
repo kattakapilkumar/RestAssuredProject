e20b4f442b99a02942b954cda8ab5e6a340ecd3b
kattakak <kapilkatta123@gmail.com>
2021-11-04 12:56:02 +0530

    restassured

---
+++ src/test/java/Test01_GET.java
@@ -0,0 +1,35 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Test01_GET {
+	
+	
+	@Test
+	void test01() {
+		
+		Response resposeob = null;
+		resposeob = RestAssured.get("https://reqres.in/api/users/2");
+		
+//		resposeob.asString();
+//		resposeob.getBody();
+//		resposeob.getStatusCode();
+		
+		System.out.println(resposeob.asString());
+		System.out.println(resposeob.getBody().asString());
+		System.out.println(resposeob.getStatusCode());
+		System.out.println(resposeob.getStatusLine());
+		System.out.println(resposeob.getHeader("content-type"));
+		System.out.println(resposeob.getTime());
+		
+		int statusCode = resposeob.getStatusCode();
+		
+		Assert.assertEquals(statusCode,200);
+		
+		
+		
+	}
+
+}
