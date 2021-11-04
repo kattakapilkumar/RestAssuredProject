1dbef9e943224c5709c2a0f180842aca10c5196e
kattakak <kapilkatta123@gmail.com>
2021-11-04 13:04:57 +0530

    BDD

--- src/test/java/RestStaticExample.java
+++ src/test/java/RestStaticExample.java
@@ -1,40 +1,40 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 
-public class Test01_GET {
-	
-	
+public class RestStaticExample {
+
+
 	@Test
 	void test01() {
-		
+
 		Response resposeob = null;
-		resposeob = RestAssured.get("https://reqres.in/api/users/2");
-		
-//		resposeob.asString();
-//		resposeob.getBody();
-//		resposeob.getStatusCode();
-		
+		resposeob = get("https://reqres.in/api/users/2");
+
+		//		resposeob.asString();
+		//		resposeob.getBody();
+		//		resposeob.getStatusCode();
+
 		System.out.println(resposeob.asString());
 		System.out.println(resposeob.getBody().asString());
 		System.out.println(resposeob.getStatusCode());
 		System.out.println(resposeob.getStatusLine());
 		System.out.println(resposeob.getHeader("content-type"));
 		System.out.println(resposeob.getTime());
-		
+
 		int statusCode = resposeob.getStatusCode();
-		
+
 		Assert.assertEquals(statusCode,200);	
 	}
-	
-//	@Test
-//	void test02() {
-//		
-//		given().
-//		
-//		
-//	}
+
+	@Test
+	void test02() {
+
+		given().get("https://reqres.in/api/users/2").then().statusCode(200);
+
+
+	}
 
 }
