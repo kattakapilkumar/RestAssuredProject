93acf055395f10aabe6100c2386ff73c73f6352f
kattakak <kapilkatta123@gmail.com>
2021-12-08 14:15:50 +0530

    content-header

--- src/test/java/RestStaticExample.java
+++ src/test/java/RestStaticExample.java
@@ -5,39 +5,35 @@ import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 
 public class RestStaticExample {
-
-
+	
+	Response resposeob = null;
 	@Test
 	void test01() {
-
 		Response resposeob = null;
 		resposeob = get("https://reqres.in/api/users/2");
-
 		//		resposeob.asString();
 		//		resposeob.getBody();
 		//		resposeob.getStatusCode();
-
 		System.out.println(resposeob.asString());
 		System.out.println(resposeob.getBody().asString());
 		System.out.println(resposeob.getStatusCode());
 		System.out.println(resposeob.getStatusLine());
 		System.out.println(resposeob.getHeader("content-type"));
 		System.out.println(resposeob.getTime());
-
 		int statusCode = resposeob.getStatusCode();
-
 		Assert.assertEquals(statusCode,200);	
 	}
-
 	@Test
 	void test02() {
-
-		given().get("https://reqres.in/api/users/2")
-		.then()
-			.statusCode(200);
-			
-
-
+		Response resposeob = 
+		given()
+        .header("User-Agent", "PostmanRuntime/7.6.0")
+		.get("https://reqres.in/api/users/2");
+		System.out.println(resposeob.getStatusCode());
+		System.out.println(resposeob.getStatusLine());	
+		System.out.println("i am here");
+		System.out.println(resposeob.getHeader("content-type"));
+//		.then()
+//			.statusCode(200);
 	}
-
 }
--- src/test/java/Test01_GET.java
+++ src/test/java/Test01_GET.java
@@ -5,8 +5,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class Test01_GET {
-	
-	
+		
 	@Test
 	void test01() {
 		
